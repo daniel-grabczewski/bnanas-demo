8da24fcd6231674ea3256efafe0e392181b402a3
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-10-27 11:06:18 +1300

    fix footer

--- client/components/Footer/Footer.tsx
+++ client/components/Footer/Footer.tsx
@@ -1,21 +1,6 @@
-import { useAuth0 } from '@auth0/auth0-react'
 import { Link } from 'react-router-dom'
 
 function Footer() {
-  const { isAuthenticated, logout, loginWithRedirect } = useAuth0()
-
-  function handleLogin() {
-    loginWithRedirect({
-      authorizationParams: {
-        redirect_uri: `${window.location.origin}/`,
-      },
-    })
-  }
-
-  function handleLogout() {
-    logout({ logoutParams: { returnTo: window.location.origin } })
-  }
-
   return (
     <footer className="footer">
       <p className="footer__copyright">
@@ -34,18 +19,6 @@ function Footer() {
         <li>
           <Link to="/contact">contact</Link>
         </li>
-
-        {!isAuthenticated && (
-          <li>
-            <button onClick={handleLogin}>log in</button>
-          </li>
-        )}
-
-        {isAuthenticated && (
-          <li>
-            <button onClick={handleLogout}>log out</button>
-          </li>
-        )}
       </ul>
     </footer>
   )
