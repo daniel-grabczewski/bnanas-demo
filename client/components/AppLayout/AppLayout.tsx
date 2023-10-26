4c932787002658fbe937d6f860dedc824d3eebdd
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-10-27 11:51:02 +1300

    page scrolls to top whenever user enters new page

--- client/components/AppLayout/AppLayout.tsx
+++ client/components/AppLayout/AppLayout.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
 import Nav from '../Nav/Nav'
 import Footer from '../Footer/Footer'
 
 function AppLayout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    // Scroll to top whenever pathname changes
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <div className="page">
       <Nav />
