853276a378e06a5cd977f8b52e33c9b570930d75
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-10-30 14:02:04 +1300

    AppLayout works with hash urls

--- client/components/AppLayout/AppLayout.tsx
+++ client/components/AppLayout/AppLayout.tsx
@@ -1,16 +1,29 @@
 import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
 import Nav from '../Nav/Nav'
 import Footer from '../Footer/Footer'
 
 function AppLayout() {
   const { pathname } = useLocation()
+  const navigate = useNavigate()
 
   useEffect(() => {
     // Scroll to top whenever pathname changes
     window.scrollTo(0, 0)
   }, [pathname])
+
+  useEffect(() => {
+    const hash = window.location.hash
+    if (hash) {
+      const path = hash.substring(1) // Remove the leading '#'
+      navigate(path)
+      // Clear the hash
+      window.location.hash = ''
+    }
+  }, [navigate])
+
+  
 
   return (
     <div className="page">
